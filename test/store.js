e00e4930cdb2d9958f76bb2dfe7b4634fb470ae1
Haris <hk12@live.ca>
2016-08-12 12:47:42 +0000

    began tests for store.js

--- test/store.js
+++ test/store.js
@@ -1,10 +1,34 @@
-var should = require('should');
-// var store = require('../app/store');
+const fs = require('fs');
+const should = require('should');
+const store = require('../app/store');
 
-describe('File Handler', function() {
-   describe('Opening File Test', function() {
-       it('should pass', function() {
-           (1).should.be.a.Number();
-       });
-   });
+const JSON_STRING = JSON.stringify([1, 2, {three: 'four'}]);
+const INVALID_JSON_STRING = '[1, 2, {three:"four"}, ]'
+
+// TODO
+// TODO
+// TODO
+
+describe('store.js', function() {
+    describe('Opening a file', function() {
+        describe('which exists', function() {
+            describe('has data which is not JSON', function() {
+                it('throws an exception', function() {
+                   
+                });
+            });
+           
+            it('has data which is JSON', function() {
+               it('retrieves the data successfully in JSON format', function() {
+                   
+               });
+            });
+        });
+       
+        describe("which doesn't exists", function() {
+            it('', function() {
+               
+            })
+        });
+    });
 });
