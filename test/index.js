dc41c60a381be17648fd96fd889e69d98d7636ee
Haris Khan <hk12@live.ca>
2017-01-25 00:09:39 -0500

    Configurating Travis.yml

---
+++ test/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const should = require('should');
+
+describe('Testing Something', function() {
+  describe('Test 1', function() {
+    describe("Test 1.1", function() {
+      it('Passes', function() {
+      });
+    })
+    describe("Test 1.2", function() {
+      it('Passes', function() {
+
+      });
+    })
+  })
+})
